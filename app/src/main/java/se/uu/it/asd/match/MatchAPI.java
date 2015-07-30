f30a2ad0dcc714431d32f61b67402f86bd279c3c
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2015-07-30 16:36:18 +0200

    initial skeleton
    add some initial skeleton of the match system. it might change quite
    a lot but i have to start from somewhere :)

---
+++ app/src/main/java/se/uu/it/asd/match/MatchAPI.java
@@ -0,0 +1,13 @@
+package se.uu.it.asd.match;
+
+/**
+ * Created by kikofernandezreyes on 30/07/15.
+ */
+public class MatchAPI {
+    public final static String URL = "http://";
+
+
+    public static final void fetchListTasks(){
+
+    }
+}
---
+++ app/src/main/java/se/uu/it/asd/match/MyActivity.java
@@ -0,0 +1,85 @@
+package se.uu.it.asd.match;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MyActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "se.uu.it.asd.match.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity_resource, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void fetchList(View view) {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connManager.getActiveNetworkInfo().isConnected()) {
+            MatchAPI.fetchListTasks();
+            buildAlertDialog("Download", "Works");
+        } else {
+            buildAlertDialog("Error", "No internet connection");
+        }
+    }
+
+    void buildAlertDialog(String title, String message){
+        AlertDialog dialog = new AlertDialog.Builder(this).create();
+        dialog.setTitle(title);
+        dialog.setMessage(message);
+        dialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        dialog.show();
+    }
+}
