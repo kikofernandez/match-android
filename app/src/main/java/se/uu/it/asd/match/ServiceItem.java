c8819fb5e71cf2020fe545c23b5805f0f4c043d5
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2015-08-19 21:09:40 +0200

    update request information view
    update the request information view to use a static xml view instead of
    creating the view dynamically.

--- app/src/main/java/se/uu/it/asd/match/ServiceItem.java
+++ app/src/main/java/se/uu/it/asd/match/ServiceItem.java
@@ -8,6 +8,7 @@ import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import se.uu.it.asd.match.beans.RowItemService;
@@ -16,34 +17,25 @@ import se.uu.it.asd.match.beans.RowItemService;
  * Created by kikofernandezreyes on 19/08/15.
  */
 public class ServiceItem extends AppCompatActivity {
+    private ImageView mImage;
+    private TextView mRequestInfoUser, mRequestInfoDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.request_info);
+
+        mImage = (ImageView) findViewById(R.id.request_info_icon);
+        mRequestInfoUser = (TextView) findViewById(R.id.request_info_user);
+        mRequestInfoDescription = (TextView) findViewById(R.id.request_info_description);
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         RowItemService row = (RowItemService) bundle.getParcelable(MyActivity.REQUEST_INFO);
 
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(row.getRequest());
-
-        setContentView(textView);
+        mImage.setImageResource(R.drawable.ic_contact_picture);
+        mRequestInfoUser.setText(row.getUser_request());
+        mRequestInfoDescription.setText(row.getRequest());
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
